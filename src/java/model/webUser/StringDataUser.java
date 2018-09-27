51014cf3ec71a3b0b586c7da4ad40b28b0fafcaf
Nick parrell <nbparrell@gmail.com>
2018-09-27 02:24:45 -0400

    HW4 Display Data

---
+++ src/java/model/webUser/StringDataUser.java
@@ -0,0 +1,108 @@
+package model.webUser;
+
+import dbUtils.FormatUtils;
+import java.sql.ResultSet;
+
+
+/* The purpose of this class is just to "bundle together" all the 
+ * character data that the user might type in when they want to 
+ * add a new Customer or edit an existing customer.  This String
+ * data is "pre-validated" data, meaning they might have typed 
+ * in a character string where a number was expected.
+ * 
+ * There are no getter or setter methods since we are not trying to
+ * protect this data in any way.  We want to let the JSP page have
+ * free access to put data in or take it out. */
+public class StringDataUser {
+
+    public String campaign_session_posting_name;
+    public String Signed_Up;
+    public String Notes;
+    public String campaign_session_posting_id;
+    public String Name;
+    public String Session_Location;
+    public String Campaign;
+    public String Session_Date;
+    public String campaign_session_id;
+    public String Description;
+    public String webUserId;
+    public String userEmail;
+    public String userPassword;
+    public String birthday;
+    public String membershipFee;
+    public String userRoleId;   // Foreign Key
+    public String userRoleType; // getting it from joined user_role table.
+
+    public String errorMsg;
+
+    // default constructor leaves all data members with empty string (Nothing null).
+    public StringDataUser() {
+    }
+
+    // overloaded constructor sets all data members by extracting from resultSet.
+    public StringDataUser(ResultSet results, int type) {
+        if (type == 0) {
+            try {
+                this.userEmail = FormatUtils.formatString(results.getObject("user_email"));
+                this.userPassword = FormatUtils.formatString(results.getObject("user_password"));
+                this.birthday = FormatUtils.formatDate(results.getObject("birthday"));
+                this.membershipFee = FormatUtils.formatDollar(results.getObject("membership_fee"));
+                this.userRoleType = FormatUtils.formatString(results.getObject("user_role_type"));
+            } catch (Exception e) {
+                this.errorMsg = "Exception thrown in model.webUser.StringData (the constructor that takes a ResultSet): " + e.getMessage();
+            }
+        } else if (type == 1) {
+            try {
+                this.userEmail = FormatUtils.formatString(results.getObject("user_email"));
+                this.userPassword = FormatUtils.formatString(results.getObject("user_password"));
+                this.birthday = FormatUtils.formatDate(results.getObject("birthday"));
+                this.membershipFee = FormatUtils.formatDollar(results.getObject("membership_fee"));
+                this.campaign_session_posting_name = FormatUtils.formatString(results.getObject("campaign_session_posting_name"));
+                this.Signed_Up = FormatUtils.formatDate(results.getObject("campaign_session_posting_created_date"));
+                this.Notes = FormatUtils.formatString(results.getObject("campaign_session_posting_desc"));
+                this.Session_Location = FormatUtils.formatString(results.getObject("campaign_session_location"));
+                this.Session_Date = FormatUtils.formatDate(results.getObject("campaign_session_date"));
+                this.Campaign = FormatUtils.formatString(results.getObject("campaign_session_campaign"));
+                this.Description = FormatUtils.formatString(results.getObject("campaign_session_desc"));
+                this.Name = FormatUtils.formatString(results.getObject("campaign_session_name"));
+
+                {
+
+                }
+            } catch (Exception e) {
+                this.errorMsg = "Exception thrown in model.webUser.StringData (the constructor that takes a ResultSet): " + e.getMessage();
+            }
+        } else if (type == 2) {
+            try {
+                
+                this.Session_Location = FormatUtils.formatString(results.getObject("campaign_session_location"));
+                this.Session_Date = FormatUtils.formatDate(results.getObject("campaign_session_date"));
+                this.Campaign = FormatUtils.formatString(results.getObject("campaign_session_campaign"));
+                this.Description = FormatUtils.formatString(results.getObject("campaign_session_desc"));
+                this.Name = FormatUtils.formatString(results.getObject("campaign_session_name"));
+                //this.campaign_session_posting_name = FormatUtils.formatString(results.getObject("campaign_session_posting_name"));
+                //this.campaign_session_posting_created_date = FormatUtils.formatDate(results.getObject("campaign_session_posting_created_date"));
+                //this.campaign_session_posting_desc = FormatUtils.formatString(results.getObject("campaign_session_posting_desc"));
+                //this.campaign_session_posting_id = FormatUtils.formatInteger(results.getObject("campaign_session_posting_id"));
+            } catch (Exception e) {
+                this.errorMsg = "Exception thrown in model.webUser.StringData (the constructor that takes a ResultSet): " + e.getMessage();
+            }
+        }
+    }
+
+    public int getCharacterCount() {
+        String s = this.webUserId + this.userEmail + this.userPassword + this.birthday
+                + this.membershipFee + this.userRoleId + this.userRoleType;
+        return s.length();
+    }
+
+    public String toString() {
+        return "Web User Id:" + this.webUserId
+                + ", User Email: " + this.userEmail
+                + ", User Password: " + this.userPassword
+                + ", Birthday: " + this.birthday
+                + ", Membership Fee: " + this.membershipFee
+                + ", User Role Id: " + this.userRoleId
+                + ", User Role Type: " + this.userRoleType;
+    }
+}
---
+++ src/java/view/WebUserView.java
@@ -0,0 +1,114 @@
+package view;
+
+// classes imported from java.sql.*
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.webUser.*;
+
+// classes in my project
+import dbUtils.*;
+
+public class WebUserView {
+
+    public static StringDataList listUsersAPI(DbConn dbc) {
+        int type = 0;
+        //PreparedStatement stmt = null;
+        //ResultSet results = null;
+        StringDataList sdl = new StringDataList();
+        try {
+            String sql = "SELECT web_user_id, "
+                    + "user_email, "
+                    + "user_password, "
+                    + "membership_fee, "
+                    + "birthday, "
+                    + "web_user.user_role_id, "
+                    + "user_role_type "
+                    + "FROM web_user, user_role where web_user.user_role_id = user_role.user_role_id "
+                    + "ORDER BY web_user_id ";  // you always want to order by something, not just random order.
+            PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                sdl.add(results, type);
+            }
+            results.close();
+            stmt.close();
+        } catch (Exception e) {
+            StringDataUser sd = new StringDataUser();
+            sd.errorMsg = "Exception thrown in WebUserView.allUsersAPI(): " + e.getMessage();
+            sdl.add(sd);
+        }
+        return sdl;
+    }
+
+    public static StringDataList listAssocAPI(DbConn dbc) {
+        int type = 1;
+        //PreparedStatement stmt = null;
+        //ResultSet results = null;
+        StringDataList sdl = new StringDataList();
+        try {
+            String sql = "SELECT campaign_session_posting_name, "
+                    + "campaign_session_posting_created_date, "
+                    + "campaign_session_posting_desc, "
+                    + "campaign_session_posting_id, "
+                    + "campaign_session_location, "
+                    + "campaign_session_campaign, "
+                    + "campaign_session_date, "
+                    + "campaign_sessions.campaign_session_id, "
+                    + "web_user.web_user_id, "
+                    + "user_email, "
+                    + "user_password, "
+                    + "membership_fee, "
+                    + "birthday, "
+                    + "user_role_id, "
+                    + "campaign_session_name,"
+                    + "campaign_session_desc "
+                    + "FROM web_user, campaign_sessions, campaign_session_posting "
+                    + "where campaign_session_posting.web_user_id = web_user.web_user_id "
+                    + "AND campaign_session_posting.campaign_session_id = campaign_sessions.campaign_session_id "
+                    + "ORDER BY campaign_session_posting_name";  // you always want to order by something, not just random order.
+            PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                sdl.add(results, type);
+            }
+            results.close();
+            stmt.close();
+        } catch (Exception e) {
+            StringDataUser sd = new StringDataUser();
+            sd.errorMsg = "Exception thrown in WebUserView.listAssocAPI(): " + e.getMessage();
+            sdl.add(sd);
+        }
+        return sdl;
+    }
+
+    public static StringDataList listOtherAPI(DbConn dbc) {
+        int type = 2;
+        //PreparedStatement stmt = null;
+        //ResultSet results = null;
+        StringDataList sdl = new StringDataList();
+        try {
+            String sql = "SELECT "
+                    + "campaign_session_location, "
+                    + "campaign_session_campaign, "
+                    + "campaign_session_date, "
+                    + "campaign_sessions.campaign_session_id,  "
+                    + "campaign_session_name,"
+                    + "campaign_session_desc "
+                    + "FROM campaign_sessions "
+                    + "ORDER BY campaign_session_date ASC";  // you always want to order by something, not just random order.
+            PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+            ResultSet results = stmt.executeQuery();
+            while (results.next()) {
+                sdl.add(results, type);
+            }
+            results.close();
+            stmt.close();
+        } catch (Exception e) {
+            StringDataUser sd = new StringDataUser();
+            sd.errorMsg = "Exception thrown in WebUserView.allUsersAPI(): " + e.getMessage();
+            sdl.add(sd);
+        }
+        return sdl;
+    }
+
+}
